b3767d72b04beba7ff2c7632b17d23f224b80702
esteloabell <manitobaaa@gmail.com>
2021-05-21 08:07:10 -0500

    Create Home.js

---
+++ screens/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Text
+} from 'react-native';
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Home;
