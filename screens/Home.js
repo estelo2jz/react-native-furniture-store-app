a1d007f942529bedb63ea2837b15064f4b7157de
esteloabell <manitobaaa@gmail.com>
2021-05-21 08:54:51 -0500

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -14,4 +14,21 @@ const Home = () => {
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.white.
+  },
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+    elevation: 9,
+  }
+})
+
 export default Home;
