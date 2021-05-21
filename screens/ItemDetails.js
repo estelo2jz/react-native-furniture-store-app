109cef620125ec67cff8b0fe7cce4a481a9f8aac
esteloabell <manitobaaa@gmail.com>
2021-05-21 08:07:03 -0500

    Create ItemDetails.js

---
+++ screens/ItemDetails.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+
+const ItemDetails = () => {
+  return (
+    <View>
+      <Text>ItemDetails</Text>
+    </View>
+  )
+}
+
+export default ItemDetails;
